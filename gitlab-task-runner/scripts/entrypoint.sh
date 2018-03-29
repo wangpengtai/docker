dbee29adcd0c8e1b4475dd8ff27e7732f0cce4d9
Ahmad Posten <ahmad.hassan612@gmail.com>
2018-03-29 15:10:58 +0200

    Add set-config

--- gitlab-task-runner/scripts/entrypoint.sh
+++ gitlab-task-runner/scripts/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+set -e
+/scripts/set-config "${CONFIG_TEMPLATE_DIRECTORY}" "${CONFIG_DIRECTORY:=$CONFIG_TEMPLATE_DIRECTORY}"
+
 cd /home/git/gitlab;
 echo "Attempting to run rake task $@";
 
