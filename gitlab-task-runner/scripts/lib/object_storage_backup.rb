cb3b1398887bb92ab5d6d3826060813aed7d162b
Balasankar "Balu" C <balasankar@gitlab.com>
2018-09-24 14:06:00 +0530

    Skip pushing existing files to object storage

--- gitlab-task-runner/scripts/lib/object_storage_backup.rb
+++ gitlab-task-runner/scripts/lib/object_storage_backup.rb
@@ -60,37 +60,6 @@ class ObjectStorageBackup
     failure_abort(output) unless status.zero?
   end
 
-  def backup_existing
-    backup_file_name = "#{@name}.#{Time.now.to_i}"
-    cmd = %W(s3cmd sync s3://#{@remote_bucket_name} s3://#{@tmp_bucket_name}/#{backup_file_name}/)
-    output, status = run_cmd(cmd)
-
-    failure_abort(output) unless status.zero?
-  end
-
-  def cleanup
-    cmd = %W(s3cmd del --force --recursive s3://#{@remote_bucket_name})
-    output, status = run_cmd(cmd)
-    failure_abort(output) unless status.zero?
-  end
-
-  def restore_from_backup
-    extracted_tar_path = File.join(File.dirname(@local_tar_path), "/tmp/#{@name}")
-    FileUtils.mkdir_p(extracted_tar_path, mode: 0700)
-
-    failure_abort("#{@local_tar_path} not found") unless File.exist?(@local_tar_path)
-
-    untar_cmd = %W(tar -xf #{@local_tar_path} -C #{extracted_tar_path})
-
-    output, status = run_cmd(untar_cmd)
-
-    failure_abort(output) unless status.zero?
-
-    Dir.glob("#{extracted_tar_path}/*").each do |file|
-     upload_to_object_storage(file)
-    end
-  end
-
   def run_cmd(cmd)
     _, stdout, wait_thr = Open3.popen2e(*cmd)
     return stdout.read, wait_thr.value.exitstatus
